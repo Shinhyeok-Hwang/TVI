717ae06c707c1f09c67b7e2de14607ea6f2c53b1
성수 <lomotos10@gmail.com>
2019-11-02 02:34:33 +0900

    var

--- contents/background.js
+++ contents/background.js
@@ -2,14 +2,14 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if(changeInfo.url == "chrome://newtab/"){
     chrome.storage.local.get(['activated', 'thanos_vacation', 'date'],
       function(result){
-        thanos_vacation = result.thanos_vacation;
+        var thanos_vacation = result.thanos_vacation;
         thanos_vacation *= 1;
-        activated = result.activated;
+        var activated = result.activated;
         if(!result.hasOwnProperty('thanos_vacation'))
           thanos_vacation = 30;
         if(!result.hasOwnProperty('activated'))
           activated = 1;
-        date = result.date;
+        var date = result.date;
 
         if(!result.hasOwnProperty('date') || (new Date()).getTime() - date >= (thanos_vacation * 60 * 1000)){
           activated = 1;
